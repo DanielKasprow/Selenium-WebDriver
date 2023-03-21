87419521b3856faf6fb91b35eb9442bc9c1cc7e5
Daniel Kasprów <127341@student.uni.opole.pl>
2023-03-21 12:52:37 +0100

    Section 21 finished

--- introduction/src/danielkasprowacadeny/TestComponents/BaseTest.java
+++ introduction/src/danielkasprowacadeny/TestComponents/BaseTest.java
@@ -1,15 +1,23 @@
 package danielkasprowacadeny.TestComponents;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Properties;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import danielkasprowacadeny.pageobjects.LandingPage;
 
@@ -36,6 +44,21 @@ public class BaseTest {
 		return driver;
 		
 	}
+	
+	public List<HashMap<String, String>> getJsonDataToMap(String filePath) throws IOException
+	{
+		String jsonContent = FileUtils.readFileToString(new File(filePath),
+				StandardCharsets.UTF_8);
+		
+		ObjectMapper mapper = new ObjectMapper();
+		List<HashMap<String, String>> data = mapper.readValue(jsonContent, new TypeReference<List<HashMap<String, String>>>(){
+		
+		});
+		
+		return data;
+		
+	}
+	
 
 	@BeforeMethod(alwaysRun = true)
 	public LandingPage launchApplication() throws IOException {
--- introduction/src/framework/StandAloneTest.java
+++ introduction/src/framework/StandAloneTest.java
@@ -1,10 +1,15 @@
 package framework;
 
+import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
+import java.util.HashMap;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -12,6 +17,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import danielkasprowacadeny.TestComponents.BaseTest;
@@ -23,10 +29,11 @@ import danielkasprowacadeny.pageobjects.OrdersPage;
 import danielkasprowacadeny.pageobjects.ProductCatalogue;
 
 public class StandAloneTest extends BaseTest {
+	
 	String productName = "ZARA COAT 3";
-
-	@Test
-	public void submitOrder() throws IOException {
+	
+	@Test(dataProvider="getData", groups = {"Purchase"})
+	public void submitOrder(HashMap<String,String> input) throws IOException {
 
 		// LandingPage landingPage = launchApplication();
 		// WebDriver driver = new EdgeDriver();
@@ -35,11 +42,11 @@ public class StandAloneTest extends BaseTest {
 
 		// LandingPage landingPage = new LandingPage(driver);
 		// landingPage.goTo();
-		ProductCatalogue productCatalogue = landingPage.loginApplication("anshika@gmail.com", "Iamking@000");
+		ProductCatalogue productCatalogue = landingPage.loginApplication(input.get("email"), input.get("password"));
 
-		productCatalogue.addProductToCart(productName);
+		productCatalogue.addProductToCart(input.get("product"));
 		CartPage cartPage = productCatalogue.goToCartPage();
-		Boolean match = cartPage.VerifyProductDisplay(productName);
+		Boolean match = cartPage.VerifyProductDisplay(input.get("product"));
 
 		Assert.assertTrue(match);
 
@@ -61,4 +68,29 @@ public class StandAloneTest extends BaseTest {
 
 	}
 
+	public String getScreenshot(String testCaseName) throws IOException {
+		TakesScreenshot ts = (TakesScreenshot)driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		File file = new File( "C:\\Users\\trane\\Desktop\\selenium_kurs\\"+ testCaseName +"\\screenshoot.png");
+		FileUtils.copyFile(source, file);
+		
+		return "C:\\Users\\trane\\Desktop\\selenium_kurs\\"+ testCaseName +"\\screenshoot.png";
+	}
+	
+	@DataProvider
+	public Object[][] getData() throws IOException {
+		
+		/*HashMap<String,String> map = new HashMap<String,String>();
+		map.put("email", "anshika@gmail.com");
+		map.put("password", "Iamking@000");
+		map.put("product", "ZARA COAT 3");
+		HashMap<String,String> map1 = new HashMap<String,String>();
+		map1.put("email", "shetty@gmail.com");
+		map1.put("password", "Iamking@000");
+		map1.put("product", "ADIDAS ORIGINAL");*/
+		
+		List<HashMap<String,String>> data = getJsonDataToMap(System.getProperty("user.dir")+"/src/danielkasprowacademy/data/PurchaseOrder.json");
+		return new Object[][] {{data.get(0)}, {data.get(1)}};
+		
+	}
 }
